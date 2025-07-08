a887014c3ed23631ba4cd4145ebccea4443b16f8
jithsungh <jithsunghsai@gmail.com>
2025-07-09 00:17:04 +0530

    Minor production changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "http://localhost:5433/api",
+  baseURL: "https://newzzy-ynxa.onrender.com/api",
   timeout: 15000,
   withCredentials: true,
   headers: {
